606948918397db116672ec863d7e4a248fb0d8fd
daniels <danielsv2008@gmail.com>
2026-05-25 10:53:20 +0300

    create function fixed

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -12,6 +12,7 @@ class Product extends Model
     protected $fillable = [
         'name',
         'description',
+        'price',
         'category',
         'status',
     ];
--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -5,7 +5,7 @@
     <a href="{{ route('products.index') }}" class="btn btn-back">Back to List</a>
     <br><br>
 
-    <form action="{{ route('products.create') }}" method="POST" style="max-width: 500px;">
+    <form action="{{ route('products.index') }}" method="POST" style="max-width: 500px;">
         @csrf
         <div class="form-group">
             <label>Name:</label>
